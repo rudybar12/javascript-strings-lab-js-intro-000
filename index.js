e9403b84201adaff35a4daed80e708fdf7d078a2
rodolfo Barrientos <rudybar12@yahoo.com>
2020-06-11 17:23:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetingSpecialGuest = "" + "specialGuest" + "!";
+var greetSpecialGuest = "Hello, everbody" + "specialGuest" + "!";
 
 var topic = "space";
 
